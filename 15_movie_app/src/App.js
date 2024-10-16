3b71048cdcc7724b409e078cb9b3484b00c7a2df
Eonjoo-Oh <eoh@student.42seoul.kr>
2024-10-16 14:46:11 +0900

    set date

--- 15_movie_app/src/App.js
+++ 15_movie_app/src/App.js
@@ -3,12 +3,20 @@ import { useState, useEffect } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
+  const getMovies = async() => {
+    const date = new Date();
+    date.setDate(date.getDate() - 1);
+    const formattedDate = date.toISOString().split('T')[0].replace(/-/g, '');
+    const response = await fetch(
+      `http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=${process.env.REACT_APP_MOVIE_API_KEY}&targetDt=${formattedDate}`
+    );
+    const json = await response.json();
+    setMovies(json.boxOfficeResult.dailyBoxOfficeList);
+    setLoading(false);
+  };
   useEffect(() => {
-    fetch(`http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=${process.env.REACT_APP_MOVIE_API_KEY}&targetDt=20120101`)
-    .then((response) => response.json())
-    .then(json => setMovies(json.boxOfficeResult.dailyBoxOfficeList));
-    // .then(json => console.log(json));
-  },[])
+    getMovies();
+  }, []);
   console.log(movies);
   return (
     <div>
